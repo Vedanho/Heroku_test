0eced17792e3270ae9ae46c0cb308c20cae40415
Inasalamov Amirkhan <104070261+Vedanho@users.noreply.github.com>
2023-05-05 15:56:59 +0300

    asfasg

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import logo from './logo.svg';
-import React, { useState, useMemo, useContext, useEffect, useRef } from "react";
+import React, { useState, useMemo, useContext, useEffect, useRef, useLayoutEffect } from "react";
 import './App.css';
 import ReactCodeInput from "react-verification-code-input";
 import ReactModal from "react-modal";
 
 
 function App() {
-
-
+  const useWindowSize = () => {
+    const [size, setSize] = useState([0, 0]);
+    useLayoutEffect(() => {
+      function updateSize() {
+        setSize([window.innerWidth, document.documentElement.offsetHeight]);
+      }
+      window.addEventListener("resize", updateSize);
+      updateSize();
+      return () => window.removeEventListener("resize", updateSize);
+    }, []);
+    return size;
+  };
+  const isMobile = useWindowSize()[0] < 767;
 
   return (
     <div className="App">
       <div className="App-header">
         <h1>HELLO ITS ME,and i am good</h1>
-        <ReactModal isOpen={true} className="auth-modal-content"> 
+        <ReactModal isOpen={true} className="auth-modal-content"
+         onFocus={true} 
+         autoFocus={true} 
+         fieldWidth={isMobile ? 44 : 56}
+					fieldHeight={isMobile ? 44 : 56}> 
 
 
         <form className="auth-modal__form" onSubmit={(e) => e.preventDefault()}>
