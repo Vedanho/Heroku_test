5e4b562614fffaddf1450e996dbff1d79639387a
Inasalamov Amirkhan <104070261+Vedanho@users.noreply.github.com>
2023-05-09 01:26:28 +0300

    asgasg

--- src/App.js
+++ src/App.js
@@ -10,15 +10,13 @@ function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <h1>HELLO ITS ME and i tired!!!</h1>
-        <ReactModal isOpen={true} className="auth-modal-content"> 
+        <h1>HELLO ITS ME and i tired very much!!!</h1>
         <form className="auth-modal__form" onSubmit={(e) => e.preventDefault()}>
 						<ReactCodeInput
             						fieldWidth={50}
 							fields={4}
 						/>
         </form>
-        </ReactModal>
   
       </div>
     </div>
